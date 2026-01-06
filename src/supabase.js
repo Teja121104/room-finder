9137a1ff8944c0fe368abac1c46e89c2ee9e8657
Teja121104 <mr.teja121104@gmail.com>
2026-01-06 10:32:35 +0530

    GItpush

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Your Supabase project URL
-const supabaseUrl = "https://kbnyyfneoocolgyrdevd.supabase.co";
+const supabaseUrl = "https://zwmabpoglsjnmblckdkv.supabase.co";
 
 // Your Publishable / anon key (safe for frontend)
-const supabaseAnonKey = "sb_publishable_2uN-AiV9KNDgwmKnItDbeA_B8HHx1zq";
+const supabaseAnonKey = "sb_publishable_Sk6wLQEAauEQxAxn7C4qWQ_O_7s5JTz";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
