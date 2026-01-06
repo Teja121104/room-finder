8b9b02184fb8655f7f1405a3fb678b86b394a9f2
Teja121104 <mr.teja121104@gmail.com>
2026-01-06 10:04:47 +0530

    Super

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Your Supabase project URL
+const supabaseUrl = "https://kbnyyfneoocolgyrdevd.supabase.co";
+
+// Your Publishable / anon key (safe for frontend)
+const supabaseAnonKey = "sb_publishable_2uN-AiV9KNDgwmKnItDbeA_B8HHx1zq";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
